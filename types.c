e47561c963de263a31b0cc1181276508f85b8f4d
Bastien Dejean <nihilhill@gmail.com>
2013-08-30 11:02:59 +0200

    Don't place the alarm in locked state
    A mode can already be exited by pressing Escape.

--- types.c
+++ types.c
@@ -54,7 +54,7 @@ hotkey_t *find_hotkey(xcb_keysym_t keysym, xcb_button_t button, uint16_t modfiel
         abort_chain();
         return find_hotkey(keysym, button, modfield, event_type, replay_event);
     }
-    if (chained && timeout > 0)
+    if (chained && !locked && timeout > 0)
         alarm(timeout);
     PRINTF("num active %i\n", num_active);
 
