d0f1446caba4ddd2704d3c0af622b00851b8dedf
ewd340 <ewd340+github@gmail.com>
2020-07-29 19:46:05 +0100

    Added macros handling functions
    Macros are identified by a key (string to be replaced) and a replacement
    text. For example, a macro defining ANIMAL to be "white cat" may be
    used to process the following text "I have a {{ANIMAL}}" to produce the
    string: "I have a white cat"
    
    Note that macros can be defined using previously defined macros and
    expansion would work as expected.
    
    For example having defined COLOR to be "white", one can define ANIMAL to
    be "{{COLOR}} cat", and then have the string "I have a {{ANIMAL}}" be
    processed to give us "I have a white cat".

---
+++ src/macros.h
@@ -0,0 +1,26 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+// Macros Hash Table
+typedef struct {
+    void *key;
+    void *value;
+    size_t k_len;
+    size_t v_len;
+} macro_t;
+
+// A Hash Table is a dynamically allocated array of macros.
+typedef struct{
+    macro_t *entries;
+    size_t size;
+} ht_t;
+
+ht_t *ht_create(const size_t size);
+void ht_set(const ht_t *ht, const void *key, const size_t k_len, const void *value, const size_t v_len);
+void *ht_get(const ht_t *ht, const void *key, const size_t k_len);
+void ht_del(const ht_t *ht, const void *key, const size_t k_len);
+void ht_destroy(ht_t *ht);
+char *ht_process_macros(const ht_t *ht, const char *src);
+void ht_set_processed(const ht_t *ht, const char *key, const char *value);
+
+#endif /* ifndef MACROS_H */
